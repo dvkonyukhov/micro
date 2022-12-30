cabb5004ce5614b131a1db97586b75c734ad741d
d.konyuhov <w1356@yandex.ru>
2022-12-30 12:44:34 +0300

    Initial commit

---
+++ micro/src/test/java/ru/dkonyukhov/micro/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dkonyukhov.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
